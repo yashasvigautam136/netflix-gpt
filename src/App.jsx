c87388b010da61f184dae538b5218543a9ba91b9
yashasvi <yashasvi.gautam@highwirepress.com>
2025-10-16 15:09:43 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="text-3xl font-bold text-green-800">
+        HEllo world 
+
+       </div>
+    </>
+  )
+}
+
+export default App
